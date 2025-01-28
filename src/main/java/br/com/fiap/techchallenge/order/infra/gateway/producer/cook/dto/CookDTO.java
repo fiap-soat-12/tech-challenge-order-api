70d73dc5f8716f37139f77448a528d07a8cb3510
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-01-27 23:55:59 -0300

    ✨ feat: Solve Debts from Cook DTO

--- src/main/java/br/com/fiap/techchallenge/order/infra/gateway/producer/cook/dto/CookDTO.java
+++ src/main/java/br/com/fiap/techchallenge/order/infra/gateway/producer/cook/dto/CookDTO.java
@@ -1,4 +1,18 @@
 package br.com.fiap.techchallenge.order.infra.gateway.producer.cook.dto;
 
-public record CookDTO() {
+
+import br.com.fiap.techchallenge.order.domain.models.Order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record CookDTO(UUID id,
+                      Integer sequence,
+                      List<CookProductDTO> products,
+                      LocalDateTime createdAt) {
+    public CookDTO(Order order) {
+        this(order.getId(), order.getSequence(), order.getProducts().stream().map(CookProductDTO::new).toList(),
+                order.getCreatedAt());
+    }
 }
