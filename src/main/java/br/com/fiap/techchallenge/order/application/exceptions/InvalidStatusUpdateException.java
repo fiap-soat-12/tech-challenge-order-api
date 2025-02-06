4dcec833bf27c7e705b2d6b7a44f74ec055e164d
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-02-05 23:13:07 -0300

    ♻️ refactor: remove some classes

--- src/main/java/br/com/fiap/techchallenge/order/application/exceptions/InvalidStatusUpdateException.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.techchallenge.order.application.exceptions;
-
-public class InvalidStatusUpdateException extends RuntimeException {
-
-	public InvalidStatusUpdateException(String message) {
-		super(message);
-	}
-
-}
